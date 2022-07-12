b87cb2db9a768d76362d846bf81869d8b20f759a
kisalapa <kisalapa@rambler.ru>
2022-07-12 12:18:54 +0400

    refactor: changed links to imgages

--- index.js
+++
@@ -1,6 +0,0 @@
-let burger = document.querySelector('.burger'),
-    menu = document.querySelector('.header__menu');
-
-burger.addEventListener('click', function(e) {
-  menu.classList.toggle('active');
-});
